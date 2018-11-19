42b552e8e4eefd9429072e37a575b9b2e5f055ec
AshitakaSekki <constantine.sky@hotmail.com>
2018-11-19 19:03:58 +0800

    第I部分，第3章

---
+++ Part I/Chapter_04/main.cpp
@@ -0,0 +1,20 @@
+#include "d3dUtility.h"
+
+//
+// Globals
+//
+IDirect3DDevice9* Device;
+
+const int Width = 640;
+const int Height = 480;
+
+IDirect3DVertexBuffer9* Triangle_VB = 0;
+
+// Define the ColorVertex
+struct ColorVertex
+{
+	float x, y, z;
+	D3DCOLOR color;
+	static const DWORD FVF;
+};
+const DWORD ColorVertex::FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
