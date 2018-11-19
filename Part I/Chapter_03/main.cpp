337547b93120240c25f19e841aa185b350e13399
AshitakaSekki <constantine.sky@hotmail.com>
2018-11-19 16:40:24 +0800

    完成第3章
    1.在书上提供的triangle代码上加入了cube。实现了cube于triangle一起旋转。

--- Part I/Chapter_03/main.cpp
+++ Part I/Chapter_03/main.cpp
@@ -9,8 +9,11 @@ IDirect3DDevice9* Device = 0;
 const int Width  = 640;
 const int Height = 480;
 
-IDirect3DVertexBuffer9* Triangle = 0; // vertex buffer to store
-                                      // our triangle data.
+IDirect3DVertexBuffer9* Triangle_VB = 0; // vertex buffer to store
+										 // our triangle data.
+
+IDirect3DVertexBuffer9* Cube_VB = 0;	 // Cube vertex buffer.
+IDirect3DIndexBuffer9* Cube_IB = 0;		 // Cube Index buffer.
 
 //
 // Classes and Structures
@@ -37,7 +40,7 @@ const DWORD Vertex::FVF = D3DFVF_XYZ;
 bool Setup()
 {
 	//
-	// Create the vertex buffer.
+	// Create the Triangle vertex buffer.
 	//
 
 	Device->CreateVertexBuffer(
@@ -45,21 +48,95 @@ bool Setup()
 		D3DUSAGE_WRITEONLY, // flags
 		Vertex::FVF,        // vertex format
 		D3DPOOL_MANAGED,    // managed memory pool
-		&Triangle,          // return create vertex buffer
+		&Triangle_VB,          // return create vertex buffer
 		0);                 // not used - set to 0
 
 	//
-	// Fill the buffers with the triangle data.
+	// Create the Cube vertex and index buffer.
 	//
 
-	Vertex* vertices;
-	Triangle->Lock(0, 0, (void**)&vertices, 0);
+	Device->CreateVertexBuffer(
+		8 * sizeof(Vertex), // size in bytes
+		D3DUSAGE_WRITEONLY, // flags
+		Vertex::FVF,        // vertex format
+		D3DPOOL_MANAGED,    // managed memory pool
+		&Cube_VB,          // return create vertex buffer
+		0);                 // not used - set to 0
 
-	vertices[0] = Vertex(-1.0f, 0.0f, 2.0f);
-	vertices[1] = Vertex( 0.0f, 1.0f, 2.0f);
-	vertices[2] = Vertex( 1.0f, 0.0f, 2.0f);
+	Device->CreateIndexBuffer(
+		6 * 6 * sizeof(WORD), //6 vertices in each face
+		D3DUSAGE_WRITEONLY,
+		D3DFMT_INDEX16,
+		D3DPOOL_MANAGED,
+		&Cube_IB,
+		0);
 
-	Triangle->Unlock();
+	//
+	// Fill the buffers with vertex data.
+	//
+
+	Vertex* Triangle;
+	Triangle_VB->Lock(0, 0, (void**)&Triangle, 0);
+
+	Triangle[0] = Vertex(-1.0f, 0.0f, 1.0f);
+	Triangle[1] = Vertex(0.0f, 1.0f, 1.0f);
+	Triangle[2] = Vertex(1.0f, 0.0f, 1.0f);
+
+	Triangle_VB->Unlock();
+
+	Vertex* Cube;
+	Cube_VB->Lock(0, 0, (void**)&Cube, 0);
+
+	Cube[0] = Vertex(-1.0f, -1.0f, -1.0f);
+	Cube[1] = Vertex(-1.0f,  1.0f, -1.0f);
+	Cube[2] = Vertex( 1.0f,  1.0f, -1.0f);
+	Cube[3] = Vertex( 1.0f, -1.0f, -1.0f);
+	Cube[4] = Vertex(-1.0f, -1.0f,  1.0f);
+	Cube[5] = Vertex(-1.0f,  1.0f,  1.0f);
+	Cube[6] = Vertex( 1.0f,  1.0f,  1.0f);
+	Cube[7] = Vertex( 1.0f, -1.0f,  1.0f);
+
+	Cube_VB->Unlock();
+
+	WORD* Cube_Indices = 0;
+	Cube_IB->Lock(0, 0, (void**)&Cube_Indices, 0);
+
+	// front side
+	Cube_Indices[0] = 0; Cube_Indices[1] = 1; Cube_Indices[2] = 2;
+	Cube_Indices[3] = 0; Cube_Indices[4] = 2; Cube_Indices[5] = 3;
+
+	// back side
+	Cube_Indices[6] = 4; Cube_Indices[7]  = 6; Cube_Indices[8]  = 5;
+	Cube_Indices[9] = 4; Cube_Indices[10] = 7; Cube_Indices[11] = 6;
+
+	// left side
+	Cube_Indices[12] = 4; Cube_Indices[13] = 5; Cube_Indices[14] = 1;
+	Cube_Indices[15] = 4; Cube_Indices[16] = 1; Cube_Indices[17] = 0;
+
+	// right side
+	Cube_Indices[18] = 3; Cube_Indices[19] = 2; Cube_Indices[20] = 6;
+	Cube_Indices[21] = 3; Cube_Indices[22] = 6; Cube_Indices[23] = 7;
+
+	// top side
+	Cube_Indices[24] = 1; Cube_Indices[25] = 5; Cube_Indices[26] = 6;
+	Cube_Indices[27] = 1; Cube_Indices[28] = 6; Cube_Indices[29] = 2;
+
+	// bottom side
+	Cube_Indices[30] = 4; Cube_Indices[31] = 0; Cube_Indices[32] = 3;
+	Cube_Indices[33] = 4; Cube_Indices[34] = 3; Cube_Indices[35] = 7;
+
+	Cube_IB->Unlock();
+
+	//
+	// Position and aim the camera
+	//
+	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXMATRIX View;
+
+	D3DXMatrixLookAtLH(&View, &position, &target, &up);
+	Device->SetTransform(D3DTS_VIEW, &View);
 
 	//
 	// Set the projection matrix.
@@ -84,21 +161,52 @@ bool Setup()
 }
 void Cleanup()
 {
-	d3d::Release<IDirect3DVertexBuffer9*>(Triangle);
+	d3d::Release<IDirect3DVertexBuffer9*>(Triangle_VB);
+	d3d::Release<IDirect3DVertexBuffer9*>(Cube_VB);
+	d3d::Release<IDirect3DIndexBuffer9*>(Cube_IB);
 }
 
 bool Display(float timeDelta)
 {
 	if( Device )
 	{
+		// Spin the cube
+		D3DXMATRIX Rx, Ry;
+
+		// rotate 45 degrees on x-axis
+		D3DXMatrixRotationX(&Rx, D3DX_PI / 4.0f);
+
+		// increment y-rotation angle each frame
+		static float y = 0.0f;
+		D3DXMatrixRotationY(&Ry, y);
+		y += timeDelta;
+
+		// reset angle to zero when angle reaches 2*PI
+		if (y >= 2 * D3DX_PI)
+		{
+			y = 0.0f;
+		}
+
+		// combine x-axis and y-axis rotation transformations
+		D3DXMATRIX p = Rx * Ry;
+		Device->SetTransform(D3DTS_WORLD, &p);
+
+		// draw the scene
 		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
 		Device->BeginScene();
 
-		Device->SetStreamSource(0, Triangle, 0, sizeof(Vertex));
+		// draw triangle
+		Device->SetStreamSource(0, Triangle_VB, 0, sizeof(Vertex));
 		Device->SetFVF(Vertex::FVF);
 
-		// Draw one triangle.
 		Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
+		// draw cube
+		Device->SetStreamSource(0, Cube_VB, 0, sizeof(Vertex));
+		Device->SetIndices(Cube_IB);
+		Device->SetFVF(Vertex::FVF);
+
+		Device->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);
 
 		Device->EndScene();
 		Device->Present(0, 0, 0, 0);
