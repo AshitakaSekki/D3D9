fd99a85df4e24b02b648fe4e79cbd38240b8326b
AshitakaSekki <constantine.sky@hotmail.com>
2018-12-03 19:09:04 +0800

    Update Transform.hlsl

--- Part IV/Chapter_16/Transform.hlsl
+++ Part IV/Chapter_16/Transform.hlsl
@@ -12,3 +12,12 @@ struct VS_OUTPUT
     vector position : POSITION;
     vector diffuse  : COLOR;
 };
+
+VS_OUTPUT Main(VS_INPUT input)
+{
+    VS_OUTPUT output = (VS_OUTPUT) 0;
+    output.position = mul(input.position, ViewProjMatrix);
+    output.diffuse = Blue;
+
+    return output;
+}
